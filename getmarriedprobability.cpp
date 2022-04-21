17947e98bbce287165cba4898f8a29feea5289a8
Leaf Ocean <91737373+LeafOcean@users.noreply.github.com>
2022-04-21 16:32:07 +0800

    Add files via upload

---
+++ getmarriedprobability.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	long long a = 1;
+	long long b = 1;
+	int c = 1;
+	cout << "请输入被测试人的姓名（用空格连接）" << endl;
+	cin >> a;
+	cout << "你们的结婚率是100%" << endl;
+	system("pause");
+		return 0;
+}
