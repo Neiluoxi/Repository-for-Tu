d81f23278876ad0f4b90dc8fd4dc8d00e7259d11
Leaf Ocean <91737373+LeafOcean@users.noreply.github.com>
2022-04-21 16:09:23 +0800

    Add files via upload

---
+++ fj.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "该软件为上海市延安初级中学2020届特供版" << endl;
+	int number = 1;
+	cout << "请输入您的班级学号" << endl;
+	cin>>number;
+	if (number == 1437)
+	{
+		cout << "你没腹肌" << endl;
+	}
+	else
+	{
+		cout << "你有腹肌" << endl;
+	}
+	system("pause");
+	return 0;
+}
